d39c77f9cc41488f750e4e04da9fbe3d507bff96
Timo <timoxd7@gmx.de>
2017-11-15 17:49:28 +0400

    Added allmost everything related to EEPROM, deleted Defenitions.ino

--- LED_Controller/LEDController.cpp
+++ LED_Controller/LEDController.cpp
@@ -14,12 +14,24 @@ LEDController::LEDController(int pinRed, int pinGreen, int pinBlue){
   _current.red = 0;
   _current.green = 0;
   _current.blue = 0;
+  _current.brightness = 1;
 
   _current.eepromRedRead = false;
   _current.eepromGreenRead = false;
   _current.eepromBlueRead = false;
 
   _slot.set = false;
+
+  return;
+}
+
+
+void LEDController::writeColor(float red, float green, float blue, float brightness){
+  analogWrite(_pin.red * brightness * 255, red);
+  analogWrite(_pin.green * brightness * 255, green);
+  analogWrite(_pin.blue * brightness * 255, blue);
+
+  return;
 }
 
 
@@ -38,12 +50,13 @@ float LEDController::getBlue(){
 }
 
 
-bool LEDController::setEEPROMSlots(int slotRed, int slotGreen, int slotBlue){
-  if(slotRed > 0 && slotGreen > 0 && slotBlue > 0){
-    if(slotRed != slotGreen && slotRed != slotBlue && slotGreen != slotBlue){
+bool LEDController::setEEPROMSlots(int slotRed, int slotGreen, int slotBlue, int slotBrightness){
+  if(slotRed >= 0 && slotGreen >= 0 && slotBlue >= 0 && slotBrightness >= 0){
+    if((slotRed != slotGreen && slotRed != slotBlue && slotRed != slotBrightness) && (slotGreen != slotBlue && slotGreen != slotBrightness) && (slotBlue != slotBrightness)){
       _slot.red = slotRed;
       _slot.green = slotGreen;
       _slot.blue = slotBlue;
+      _slot.brightness = slotBrightness;
 
       _slot.set = true;
 
@@ -80,5 +93,120 @@ float LEDController::getRedFromEEPROM(){
   }
 
   return -1;
+}
+
+
+float LEDController::getGreenFromEEPROM(){
+  if (_current.eepromGreenRead) return _current.eepromGreen;
+  if (_slot.set){
+    EEPROM.get(_slot.green, _current.eepromGreen);
+    _current.eepromGreenRead = true;
+    return _current.eepromGreen;
+  }
+
+  return -1;
+}
+
+
+float LEDController::getBlueFromEEPROM(){
+  if (_current.eepromBlueRead) return _current.eepromBlue;
+  if (_slot.set){
+    EEPROM.get(_slot.blue, _current.eepromBlue);
+    _current.eepromBlueRead = true;
+    return _current.eepromBlue;
+  }
+
+  return -1;
+}
+
+
+float LEDController::getBrightnessFromEEPROM(){
+  if (_current.eepromBrightnessRead) return _current.eepromBrightness;
+  if (_slot.set){
+    EEPROM.get(_slot.brightness, _current.eepromBrightness);
+    _current.eepromBrightnessRead = true;
+    return _current.eepromBrightness;
+  }
+
+  return -1;
+}
+
+
+bool LEDController::forceReloadFromEEPROM(){
+  if(_slot.set){
+    EEPROM.get(_slot.red, _current.eepromRed);
+    _current.eepromRedRead = true;
+
+    EEPROM.get(_slot.green, _current.eepromGreen);
+    _current.eepromGreenRead = true;
+
+    EEPROM.get(_slot.blue, _current.eepromBlue);
+    _current.eepromBlueRead = true;
+
+    EEPROM.get(_slot.brightness, _current.eepromBrightness);
+    _current.eepromBrightnessRead = true;
+
+    return true;
+  }
+
+  return false;
+}
+
+
+void LEDController::setColor(float red, float green, float blue){
+  this->writeColor(red, green, blue, _current.brightness);
+
+  _current.red = red;
+  _current.green = green;
+  _current.blue = blue;
+
+  return;
+}
+
+
+void LEDController::setColor(float red, float green, float blue, float brightness){
+  this->writeColor(red, green, blue, brightness);
+
+  _current.red = red;
+  _current.green = green;
+  _current.blue = blue;
+  _current.brightness = brightness;
+
+  return;
+}
+
+
+bool LEDController::setFromEEPROM(){
+  if(this->getRedFromEEPROM() == -1) return false;
+  if(this->getGreenFromEEPROM() == -1) return false;
+  if(this->getBlueFromEEPROM() == -1) return false;
+  if(this->getBrightnessFromEEPROM() == -1) return false;
+
+  this->setColor(_current.eepromRed, _current.eepromGreen, _current.eepromBlue, _current.eepromBrightness);
+  return true;
+}
+
+
+void LEDController::saveToEEPROM(float red, float green, float blue, float brightness){
+  EEPROM.put(_slot.red, red);
+  EEPROM.put(_slot.green, green);
+  EEPROM.put(_slot.blue, blue);
+  EEPROM.put(_slot.brightness, brightness);
+
+  return;
+}
+
+
+void LEDController::saveCurrentToEEPROM(){
+  this->saveToEEPROM(_current.red, _current.green, _current.blue, _current.brightness);
+
+  return;
+}
+
+
+void LEDController::saveToEEPROM(){
+  this->saveCurrentToEEPROM();
+
+  return;
 }
 
--- LED_Controller/LEDController.h
+++ LED_Controller/LEDController.h
@@ -9,16 +9,21 @@ class LEDController
     float getRed();       //
     float getGreen();     //
     float getBlue();      //
-    bool setEEPROMSlots(int slotRed, int slotGreen, int slotBlue);  //
+    bool setEEPROMSlots(int slotRed, int slotGreen, int slotBlue, int slotBrightness);  //
     int getEEPROMSlotRed();   //
     int getEEPROMSlotGreen(); //
     int getEEPROMSlotBlue();  //
-    float getRedFromEEPROM(); 
-    float getGreenFromEEPROM();
-    float getBlueFromEEPROM();
-    void setColor(float red, float green, float blue);
-    void setFromEEPROM();
-    void saveToEEPROM();
+    float getRedFromEEPROM();   //
+    float getGreenFromEEPROM(); //
+    float getBlueFromEEPROM();  //
+    float getBrightnessFromEEPROM(); //
+    bool forceReloadFromEEPROM(); //
+    void setColor(float red, float green, float blue); //
+    void setColor(float red, float green, float blue, float brightness); //
+    bool setFromEEPROM(); //
+    void saveToEEPROM(float red, float green, float blue, float brightness);  //
+    void saveToEEPROM(); //
+    void saveCurrentToEEPROM(); //
     void fade1(float speed);
     void fade2(float speed);
     void fadePause();
@@ -33,19 +38,21 @@ class LEDController
     void work();
     
   private:
+    void writeColor(float red, float green, float blue, float brightness);
+    
     struct _pin {
       int red, green, blue;
     } _pin;
 
     struct _slot {
-      int red, green, blue;
+      int red, green, blue, brightness;
       bool set;
     } _slot;
 
     struct _current {
-      float red, green, blue;
-      float eepromRed, eepromGreen, eepromBlue;
-      bool eepromRedRead, eepromGreenRead, eepromBlueRead;
+      float red, green, blue, brightness;
+      float eepromRed, eepromGreen, eepromBlue, eepromBrightness;
+      bool eepromRedRead, eepromGreenRead, eepromBlueRead, eepromBrightnessRead;
     } _current;
 };
 
